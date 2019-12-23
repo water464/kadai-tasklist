28981305f33c6ff4c2a50c89a2e58ef01b7f4b9c
EC2 Default User <ec2-user@ip-172-31-92-137.ec2.internal>
2019-12-23 06:42:07 +0000

    DRY

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,12 @@
 class TasksController < ApplicationController
-    
+      before_action :set_task, only: [:show, :edit, :update, :destroy]
+      
   def index
     @tasks = Task.all
   end
 
   def show
-    @task = Task.find(params[:id])
+
   end
 
   def new
@@ -25,11 +26,11 @@ class TasksController < ApplicationController
   end
 
   def edit
-    @task = Task.find(params[:id])
+
   end
 
   def update
-    @task = Task.find(params[:id])
+
 
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
@@ -41,7 +42,7 @@ class TasksController < ApplicationController
   end
 
   def destroy
-    @task = Task.find(params[:id])
+
     @task.destroy
 
     flash[:success] = 'Task は正常に削除されました'
@@ -49,7 +50,11 @@ class TasksController < ApplicationController
   end
 end
 
+  def set_task
+    @task = Task.find(params[:id])
+  end
+  
   # Strong Parameter
   def task_params
-    params.require(:task).permit(:content)
+    params.require(:task).permit(:content, :status)
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_22_153126) do
+ActiveRecord::Schema.define(version: 2019_12_23_054027) do
 
   create_table "tasks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "status"
   end
 
 end
